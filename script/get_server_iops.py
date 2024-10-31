db63f8c0d85276aa20ba99a1e069499f185fef8b
Arjun <arjun21k@gmail.com>
2024-10-30 18:21:28 -0700

    main-x86 commit

---
+++ script/get_server_iops.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+def main():
+    import os
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Error: Filename missing")
+        exit()
+
+    file_name = str(sys.argv[1])
+    #print(file_name)
+    file1 = open(file_name, 'r')
+
+    count = 0
+    total_mops = 0
+    for line in file1:
+        #print(line)
+        if "tput=" in line:
+            #print(line)
+            temp = line.split(' ')
+            #print(temp[1])
+            if temp[1]:
+                #print(temp[1])
+                count += 1
+                total_mops += float(temp[1])
+
+    print("Average throughput = {:.2f} Mops".format(total_mops/count))
+
+if __name__ == "__main__":
+    main()
+
